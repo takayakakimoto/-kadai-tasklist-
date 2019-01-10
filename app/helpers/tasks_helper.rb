9353fa8503ce966684190d629903c4f389d7bd14
kanchi.nagao <wanzhiyongwei@gmail.com>
2019-01-10 12:23:47 +0000

    user login

--- app/helpers/tasks_helper.rb
+++
@@ -1,7 +0,0 @@
-module TasksHelper
-   def gravatar_url(task, options = { size: 80 })
-    gravatar_id = Digest::MD5::hexdigest(task.email.downcase)
-    size = options[:size]
-    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-  end
-end
