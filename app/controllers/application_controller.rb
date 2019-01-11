a4d83e7245831d64ebaf475be6c2b2b9fb3a6b2d
kanchi.nagao <wanzhiyongwei@gmail.com>
2019-01-11 06:10:17 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
-class ApplicationController < ActionController::Base
+ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
     include SessionsHelper
-  
-   private
+ private
 
   def require_user_logged_in
     unless logged_in?
       redirect_to login_url
     end
+  end
+  
+ def counts(user)
+    @count_tasks = user.tasks.count
   end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,55 +1,35 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-
-  def index
-     @tasks = Task.all.page(params[:page])
-  end
-
-  def show
-  end
-
-  def new
-    @task = Task.new
-  end
-
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
   def create
-      @task = Task.new(task_params)
-
+     @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'タスクが投稿されました'
-      redirect_to @task
+      flash[:success] = 'タスクを投稿しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'タスクが投稿されません'
-      render :new
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'タスクの投稿に失敗しました。'
+      render 'toppages/index'
     end
   end
 
-  def edit
-  end
-
-  def update
-    if @task.update(task_params)
-    flash[:success] = 'タスクが編集されました'
-    redirect_to @task
-  else
-    flash.now[:danger] = 'タスクが編集されませんでした'
-    render :edit
-  end
-end
   def destroy
-      @task.destroy
-
-  flash[:success] = 'タスクが削除されました'
-  redirect_to tasks_url
+     @task.destroy
+    flash[:success] = 'タスクを削除しました。'
+    redirect_back(fallback_location: root_path)
   end
+  
+   private
 
-private
-
-def set_task
-  @task = Task.find(params[:id])
-end
-
-def task_params
-  params.require(:task).permit(:content,:status)
-end
+  def task_params
+    params.require(:task).permit(:content)
+  end
+  
+   def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
